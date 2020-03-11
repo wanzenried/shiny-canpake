f9e8c9fb131352df9cf48a635eb5b99126289006
wanzenried <wanzenried@users.noreply.github.com>
2020-03-11 19:25:41 +0100

    Update sketch.js
    added canvas + variables for controlling size of said canvas.
    included a 10 by 10 grid just so we wouldn't have just a black background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+let canvasX = 600;
+let canvasY = 600;
+
 function setup() {
   // put setup code here
+  createCanvas(canvasX, canvasY);
 }
 
 function draw() {
   // put drawing code here
+  background(0);
+  drawGrid(10,10,255,1);
 }
