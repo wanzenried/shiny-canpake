57ccaa9c0df4db928bf17587a83230b6e05d170f
wanzenried <wanzenried@users.noreply.github.com>
2020-03-12 14:09:41 +0100

    groundrect visibility added
    - completed constructor of the groundRect class
    - completed draw function of the groundRect class
    - updated *index.html* to include the *ground.js* script
    - added code to *sketch.js* for showing a single instance of the ground class

--- ground.js
+++ ground.js
@@ -1,5 +1,14 @@
 class groundRect {
-  constructor() {
-
+  constructor(x1, y1, x2, y2, solid = true) {
+    this.topLeftCorner = createVector(x1, y1);
+    this.bottomRightCorner = createVector(x2, y2);
+    this.solid = solid;
   }
+  draw() {
+    rectMode(CORNERS);
+    fill(100);
+    noStroke();
+    rect(this.topLeftCorner.x, this.topLeftCorner.y, this.bottomRightCorner.x, this.bottomRightCorner.y);
+  }
+  update() {}
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 let canvasX = 600;
 let canvasY = 600;
+let box;
 
 function setup() {
   // put setup code here
   createCanvas(canvasX, canvasY);
+  box = new groundRect(100,100,300,250);
 }
 
 function draw() {
   // put drawing code here
   background(0);
   drawGrid(10,10,255,1);
+  box.draw();
 }
